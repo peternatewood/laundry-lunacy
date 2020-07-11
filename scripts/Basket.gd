1efa012df761486885371153d381cabf0bc897dc
peternatewood <peternatewood@tutanota.com>
2020-07-11 18:39:48 -0400

    Upend basket with mouse buttons

--- scripts/Basket.gd
+++ scripts/Basket.gd
@@ -1,29 +1,41 @@
 extends KinematicBody2D
 
 var grabbed: bool = false
+var mouse_over: bool = false
 var mouse_pos: Vector2 = Vector2.ZERO
+var target_angle: float = 0.0
 var velocity: Vector2 = Vector2.ZERO
 var upend: bool = false
 
 
 func _ready():
-	var click_area = get_node("Click Area")
-	click_area.connect("input_event", self, "_on_click_area_event")
+	var mouse_area = get_node("Mouse Area")
+	mouse_area.connect("mouse_entered", self, "_on_mouse_entered")
+	mouse_area.connect("mouse_exited", self, "_on_mouse_exited")
 
 
-func _on_click_area_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT:
-			if event.pressed:
-				emit_signal("grabbed")
+func _on_mouse_entered():
+	mouse_over = true
+
+
+func _on_mouse_exited():
+	mouse_over = false
 
 
 func _process(delta):
 	if grabbed:
-		velocity = mouse_pos - position
+		velocity = SPEED * (mouse_pos - position)
 	else:
 		# Move back to start position, or drop to ground?
+		velocity.x = lerp(velocity.x, 0, delta * SPEED)
 		velocity.y += 9.8
+		if is_on_floor():
+			velocity.y = 0
+
+	if rotation != target_angle:
+		set_rotation(lerp(rotation, target_angle, delta * ROTATION_SPEED))
+		if abs(rotation - target_angle) < 1e-2:
+			rotation = target_angle
 
 	move_and_slide(velocity)
 
@@ -37,4 +49,15 @@ func release():
 	grabbed = false
 
 
+func upend(direction: int):
+	target_angle = direction * PI / 2
+
+
+func upright():
+	target_angle = 0.0
+
+
 signal grabbed
+
+const ROTATION_SPEED: float = 3.0
+const SPEED: float = 5.0
--- scripts/SceneController.gd
+++ scripts/SceneController.gd
@@ -7,10 +7,6 @@ onready var basket = get_node(basket_path)
 var mouse_pos: Vector2 = Vector2.ZERO
 
 
-func _ready():
-	basket.connect("grabbed", self, "_on_basket_grabbed")
-
-
 func _input(event):
 	if event is InputEventMouseMotion:
 		mouse_pos = event.position
@@ -18,11 +14,19 @@ func _input(event):
 		if basket.grabbed:
 			basket.mouse_pos = mouse_pos
 	elif event is InputEventMouseButton:
-		if event.button_index == BUTTON_LEFT:
-			if not event.pressed:
-				if basket.grabbed:
-					basket.release()
-
-
-func _on_basket_grabbed():
-	basket.grab(mouse_pos)
+		match event.button_index:
+			BUTTON_LEFT:
+				if event.pressed:
+					basket.upend(-1)
+				else:
+					basket.upright()
+			BUTTON_RIGHT:
+				if event.pressed:
+					basket.upend(1)
+				else:
+					basket.upright()
+	elif event.is_action("grab"):
+		if event.pressed and basket.mouse_over:
+			basket.grab(mouse_pos)
+		elif basket.grabbed:
+			basket.release()
