5164219ddc3726da020734ad113325941bbcc693
peternatewood <peternatewood@tutanota.com>
2020-07-12 10:58:18 -0400

    Add directions popup to title scene

--- scripts/Title.gd
+++ scripts/Title.gd
@@ -1,5 +1,8 @@
 extends Control
 
+onready var directions_popup = get_node("Directions")
+onready var overlay = get_node("Overlay")
+
 
 func _ready():
 	randomize()
@@ -7,13 +10,27 @@ func _ready():
 	var start_button = get_node("PanelContainer/VBoxContainer/Start Button")
 	start_button.connect("pressed", self, "_on_start_pressed")
 
+	var directions_button = get_node("PanelContainer/VBoxContainer/Directions Button")
+	directions_button.connect("pressed", self, "_on_directions_pressed")
+
 	var quit_button = get_node("PanelContainer/VBoxContainer/Quit Button")
 	quit_button.connect("pressed", self, "_on_quit_pressed")
 
+	directions_popup.connect("popup_hide", self, "_on_directions_popup_hide")
 
-func _on_start_pressed():
-	get_tree().change_scene("res://main.tscn")
+
+func _on_directions_popup_hide():
+	overlay.hide()
+
+
+func _on_directions_pressed():
+	overlay.show()
+	directions_popup.popup_centered()
 
 
 func _on_quit_pressed():
 	get_tree().quit()
+
+
+func _on_start_pressed():
+	get_tree().change_scene("res://main.tscn")
